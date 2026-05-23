d5a1e103780c67d410c468e7ead0c0a20149b39b
Yahya D. Ahmed <yahya1alhamedi@gmail.com>
2026-05-23 17:40:34 +0300

    refactor: enforce consistent semicolon-free code style and update formatting rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-import { defineConfig, globalIgnores } from 'eslint/config';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -21,4 +21,4 @@ export default defineConfig([
     },
   },
   eslintConfigPrettier,
-]);
+])
